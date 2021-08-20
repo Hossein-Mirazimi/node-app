6b4bd7fbf9076bc587f9cf559e33867344874570
Hossein Mirazimi <hossein.ma1831@gmail.com>
2021-08-20 17:10:01 +0430

    initial express app

---
+++ server/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const PORT = 3000;
+
+const app = express();
+
+app.get('/', (req,res) => res.send('Hello World'));
+
+app.listen(PORT, () => console.log(`Server is running to port: ${PORT}`));
