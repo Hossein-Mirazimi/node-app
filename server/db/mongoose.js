ea0de3b66a3c56326bb5a522a22ab62417a2aa1d
Hossein Mirazimi <hossein.ma1831@gmail.com>
2021-08-21 23:32:44 +0430

    nothing

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,10 +2,17 @@ const mongoose = require('mongoose');
 // const config = require('config');
 const uri = process.env.MONGO_URI;
 mongoose.Promise = global.Promise;
-mongoose.connect(`mongodb+srv://root:icpcPbKtpp2CyPNi@z3f9a335d-mongodb.qovery.io/PA_DB`, {
+//root:JuBbV9RiZ0bR5DoZ
+mongoose.connect(`mongodb://z3afaf0a1-mongodb.qovery.io/PA_DB`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: true,
+}, err => {
+  if(err) {
+    console.log("Error: ",err);
+    console.log('____________________________________________________');
+    process.exit(1)
+  }
 });
 
 module.exports = {
