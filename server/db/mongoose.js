5c77bea2387b08ef9e54b4cdb422601cdc5d4d0c
Hossein Mirazimi <hossein.ma1831@gmail.com>
2021-08-21 22:54:07 +0430

    change mongo ui

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 // const config = require('config');
 const uri = process.env.MONGO_URI;
 mongoose.Promise = global.Promise;
-mongoose.createConnection(`mongodb://${uri}`, {
-  user: 'root',
-  pass: 'icpcPbKtpp2CyPNi',
+mongoose.connect(`mongodb+srv://root:icpcPbKtpp2CyPNi@z3f9a335d-mongodb.qovery.io/PA_DB`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: true,
